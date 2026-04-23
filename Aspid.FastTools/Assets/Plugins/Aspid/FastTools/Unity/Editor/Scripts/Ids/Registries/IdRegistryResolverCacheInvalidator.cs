840c3673a96b4ccf1e69df8c7334124997e3f7f4
Vladislav Panin <vpd.aspid@gmail.com>
2026-04-23 09:38:10 +0300

    Rename StringIdRegistryCacheInvalidator to IdRegistryResolverCacheInvalidator

--- Aspid.FastTools/Assets/Plugins/Aspid/FastTools/Unity/Editor/Scripts/Ids/Registries/IdRegistryResolverCacheInvalidator.cs
+++ Aspid.FastTools/Assets/Plugins/Aspid/FastTools/Unity/Editor/Scripts/Ids/Registries/IdRegistryResolverCacheInvalidator.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 // ReSharper disable once CheckNamespace
 namespace Aspid.FastTools.Ids.Editors
 {
-    internal sealed class StringIdRegistryCacheInvalidator : AssetPostprocessor
+    internal sealed class IdRegistryResolverCacheInvalidator : AssetPostprocessor
     {
         private static void OnPostprocessAllAssets(string[] imported, string[] deleted, string[] moved, string[] movedFrom)
         {
